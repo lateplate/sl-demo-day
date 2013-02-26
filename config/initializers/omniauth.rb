93d527c0ebe38078281571f072464a9f9e191d40
Will Piers <wpiers0405@gmail.com>
2013-02-25 18:22:19 -0600

    Will's first commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,15 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '521194684598530', 'a58fbed7c39dc194038dab093c81e7ae',
+   		  :scope => 'email,user_birthday,read_stream', :authorize_params => { :display => 'popup' }
+
+	def redirect_to_failure
+	  message_key = env['omniauth.error.type']
+	  new_path = "#{env['SCRIPT_NAME']}#{OmniAuth.config.path_prefix}/failure?message=#{message_key}"
+	  Rack::Response.new(["302 Moved"], 302, 'Location' => new_path).finish
+	end
+end
+
+# FACEBOOK_APP_ID='521194684598530'
+# FACEBOOK_SECRET='a58fbed7c39dc194038dab093c81e7ae'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nag::Application.config.secret_token = '17d41b4180bf70aabca4ee37eda6c819ecd8e8f6da13da2f039c6472c24fae606c24c09c1740316de7c672177413910b2a79a09d172368ad531105889d5cb697'
