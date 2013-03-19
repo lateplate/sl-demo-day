7530bc8ad436080b71a7cd96ef0d6ff31d636a84
Will Piers <wpiers0405@gmail.com>
2013-03-19 13:11:51 -0500

    added mailing information for production. moved secret info out of omniauth.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,18 @@ Sldemoday::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.assets.debug = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'nagitive',
+    :password             => 'starterleague',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,16 +10,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	  Rack::Response.new(["302 Moved"], 302, 'Location' => new_path).finish
 	end
 end
-
-# Orignal settings from Will
-# FACEBOOK_APP_ID='521194684598530'
-# FACEBOOK_SECRET='a58fbed7c39dc194038dab093c81e7ae'
-
-# localhost setup by Dan
-# FACEBOOK_APP_ID='436592866423465'
-# FACEBOOK_SECRET='d8e0bf02966a46740a7674d947d3d8e7'
-# FACEBOOK_APP_TOKEN ='436592866423465|M35GBwF47W2-xLEVys-n-tyeSws'
-
-# herokuapp.com setup by Dan
-# FACEBOOK_APP_ID='224676817673489'
-# FACEBOOK_SECRET='dee566d81462e87246a361ef7be382a3'
