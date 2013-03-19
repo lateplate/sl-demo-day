6e7e5e766ae1cf07e0af6ebf3d296a812c6254b1
Will Piers <wpiers0405@gmail.com>
2013-03-19 13:32:05 -0500

    moved sensitive info out of config/environments files and into application.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,8 @@ Sldemoday::Application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => 'nagitive',
-    :password             => 'starterleague',
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
     config.action_mailer.perform_deliveries = true
